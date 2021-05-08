5b34f5f3db7dbc549df980d4cd5ca1c5942c48f5
Zoqueteboy <63069121+Zoqueteboy@users.noreply.github.com>
2021-05-08 11:02:02 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+let Azar = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    Azar = 0
+    basic.showNumber(randint(1, 3))
+    if (Azar == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+    if (Azar == 2) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+    }
+    if (Azar == 3) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
